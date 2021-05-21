225bd65568c8c9c2bd251958fb14a154b4a7c081
Sajal Chaplot <35186539+SajalChaplot@users.noreply.github.com>
2021-05-21 22:21:44 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,4 +1,5 @@
-var settings = {
+function feed(){
+  var settings = {
   "url": "https://api.instagram.com/oauth/authorize?client_id=584068419184550&redirect_uri=https://sajalchaplot.co.in/#!/play&scope=user_profile,user_media&response_type=code",
   "method": "GET",
   "timeout": 0,
@@ -6,8 +7,9 @@ var settings = {
     "Cookie": "csrftoken=6AiATZmFz0x1sdhl2uXxZxVHMn9ADdjY; ig_did=DA9AF0F7-41E8-47A6-BCEC-4DA31717F902; ig_nrcb=1; mid=YKPsxgAEAAEJTJf3vY5ZjxdP0jil"
   },
 };
+}
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+//$.ajax(settings).done(function (response) {
+  //console.log(response);
+//});
   
