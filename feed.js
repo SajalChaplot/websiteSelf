4fa8cf73d5b1d1cb50dc03ca49d2d1bfaf028814
Sajal Chaplot <35186539+SajalChaplot@users.noreply.github.com>
2021-05-21 21:14:02 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,12 +1,13 @@
-var settings = {
-  "url": "https://api.instagram.com/oauth/authorize?client_id={{insta app id}}&redirect_uri=https://sajalchaplot.co.in/#!/&scope=user_profile,user_media&response_type=code",
-  "method": "GET",
-  "timeout": 0,
-  "headers": {
-    "Cookie": "csrftoken=6AiATZmFz0x1sdhl2uXxZxVHMn9ADdjY; ig_did=DA9AF0F7-41E8-47A6-BCEC-4DA31717F902; ig_nrcb=1; mid=YKPsxgAEAAEJTJf3vY5ZjxdP0jil"
-  },
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "csrftoken=6AiATZmFz0x1sdhl2uXxZxVHMn9ADdjY; ig_did=DA9AF0F7-41E8-47A6-BCEC-4DA31717F902; ig_nrcb=1; mid=YKPsxgAEAAEJTJf3vY5ZjxdP0jil");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
 };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+fetch("https://api.instagram.com/oauth/authorize?client_id={{insta app id}}&redirect_uri=https://oauth.pstmn.io/v1/callback&scope=user_profile,user_media&response_type=code", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
