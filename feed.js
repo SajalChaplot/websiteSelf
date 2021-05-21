c978b3f844706846dbb320926590aa4a0fb543c0
Sajal Chaplot <35186539+SajalChaplot@users.noreply.github.com>
2021-05-22 00:49:00 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,8 +1,8 @@
 var settings = {
-  "url": "https://api.instagram.com/oauth/authorize?client_id=584068419184550&redirect_uri=https://oauth.pstmn.io/v1/callback&scope=user_profile,user_media&response_type=code",
+  "url": "https://api.instagram.com/oauth/authorize?client_id=513134383145025&redirect_uri=https://sajalchaplot.co.in/#!/play&scope=user_profile,user_media&response_type=code",
   "method": "GET",
   "timeout": 0,
-  "headers": {'Access-Control-Allow-Origin': 'https://oauth.pstmn.io/v1/callback' },
+  "headers": {'Access-Control-Allow-Origin': '*' },
 };
 
 $.ajax(settings).done(function (response) {
