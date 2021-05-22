45d206d03a296be2e57df429fb749cf58979af1a
Sajal Chaplot <35186539+SajalChaplot@users.noreply.github.com>
2021-05-22 15:36:30 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,8 +1,17 @@
 function feedprofile(){
     
+    
 var url ="https://graph.instagram.com/me/media?fields=id,caption&access_token=IGQVJXdDVFZAlVZAT0J1M3BJYlNXSWpvZADhVQ3UzdWNtcXRzZAEdrdDZANYjhRRExNcVNPUzVicGNkeTNkbmlVUTB5MEZAkYXFkN04tVFJ3eTZADMWdQTHduNVNVZA3ZAIRkFkQy1QZAGxScDVR"
 ;
 $.get(url, function(data, status){
     console.log("Data: " + data + "\nStatus: " + status);
 });
 }
+
+var userFeed = new Instafeed({
+		get: 'user',
+		target: "insta",
+		limit:30,
+    	resolution: 'low_resolution',
+		accessToken: 'IGQVJXVDZAEUXMwMC1QQVBMdThsZAkJHdFJIdldUWHRCZAE1DN1hydjhqNXB6RGZABb29YQS1OUEQ0TnBPS2dvN2NsU1BrNl9VRE9xVmFQZAUI2WjJtY0w4NEpzLTdoeVNDNGFSaEltR0M1QU50MnRnVmt6YQZDZD'
+	});
