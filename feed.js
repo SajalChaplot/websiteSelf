54f25f2fe65e0faf35ccf5e233feec5556f85552
Sajal Chaplot <35186539+SajalChaplot@users.noreply.github.com>
2021-05-22 15:26:53 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,8 +1,8 @@
-function feed(){
+function feedprofile(){
     
 var url ="https://graph.instagram.com/me/media?fields=id,caption&access_token=IGQVJXdDVFZAlVZAT0J1M3BJYlNXSWpvZADhVQ3UzdWNtcXRzZAEdrdDZANYjhRRExNcVNPUzVicGNkeTNkbmlVUTB5MEZAkYXFkN04tVFJ3eTZADMWdQTHduNVNVZA3ZAIRkFkQy1QZAGxScDVR"
-
+;
 $.get(url, function(data, status){
     console.log("Data: " + data + "\nStatus: " + status);
+};
 }
-      }
