04cdb5355a50999d6f20c403d429141754047332
Sajal Chaplot <35186539+SajalChaplot@users.noreply.github.com>
2021-05-22 15:40:47 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -14,4 +14,6 @@ var userFeed = new Instafeed({
 		limit:30,
     	resolution: 'low_resolution',
 		accessToken: 'IGQVJXVDZAEUXMwMC1QQVBMdThsZAkJHdFJIdldUWHRCZAE1DN1hydjhqNXB6RGZABb29YQS1OUEQ0TnBPS2dvN2NsU1BrNl9VRE9xVmFQZAUI2WjJtY0w4NEpzLTdoeVNDNGFSaEltR0M1QU50MnRnVmt6YQZDZD'
-	});
+});
+
+userFeed.run
