533b12d886fa553431250265f4178bd1ea7c5864
Sajal Chaplot <35186539+SajalChaplot@users.noreply.github.com>
2021-05-22 15:43:39 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,13 +1,3 @@
-function feedprofile(){
-    
-    
-var url ="https://graph.instagram.com/me/media?fields=id,caption&access_token=IGQVJXdDVFZAlVZAT0J1M3BJYlNXSWpvZADhVQ3UzdWNtcXRzZAEdrdDZANYjhRRExNcVNPUzVicGNkeTNkbmlVUTB5MEZAkYXFkN04tVFJ3eTZADMWdQTHduNVNVZA3ZAIRkFkQy1QZAGxScDVR"
-;
-$.get(url, function(data, status){
-    console.log("Data: " + data + "\nStatus: " + status);
-});
-}
-
 var userFeed = new Instafeed({
 		get: 'user',
 		target: "insta",
@@ -16,4 +6,17 @@ var userFeed = new Instafeed({
 		accessToken: 'IGQVJXVDZAEUXMwMC1QQVBMdThsZAkJHdFJIdldUWHRCZAE1DN1hydjhqNXB6RGZABb29YQS1OUEQ0TnBPS2dvN2NsU1BrNl9VRE9xVmFQZAUI2WjJtY0w4NEpzLTdoeVNDNGFSaEltR0M1QU50MnRnVmt6YQZDZD'
 });
 
-userFeed.run
+function feedprofile(){
+    
+    
+var url ="https://graph.instagram.com/me/media?fields=id,caption&access_token=IGQVJXdDVFZAlVZAT0J1M3BJYlNXSWpvZADhVQ3UzdWNtcXRzZAEdrdDZANYjhRRExNcVNPUzVicGNkeTNkbmlVUTB5MEZAkYXFkN04tVFJ3eTZADMWdQTHduNVNVZA3ZAIRkFkQy1QZAGxScDVR"
+;
+$.get(url, function(data, status){
+    console.log("Data: " + data + "\nStatus: " + status);
+});
+userFeed.run	
+}
+
+
+
+
